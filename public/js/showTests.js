d6a71b6e760d152b276732ca1551edb35ce087aa
walshy <54697311+Alex-Walsh@users.noreply.github.com>
2020-02-13 11:14:14 -0500

    Beautified code

--- public/js/showTests.js
+++ public/js/showTests.js
@@ -7,27 +7,27 @@ var cheaters = [];
 var test;
 
 function showTests() {
-  db.collection("tests").doc("cdfr").get().then(function(doc){
-    test = doc.data();
-    for(var i = 0; i < test.cheaters.length;i++){
-      cheaters.push(test.cheaters[i]);
-    }
-  }).catch(function(error){
-    console.log(error);
-  });
-  var user = firebase.auth().currentUser;
-  var teachers = db.collection("teachers");
-  if (user != null) {
-    user.providerData.forEach(function(profile) {
-      email = profile.email;
+    db.collection("tests").doc("cdfr").get().then(function(doc) {
+        test = doc.data();
+        for (var i = 0; i < test.cheaters.length; i++) {
+            cheaters.push(test.cheaters[i]);
+        }
+    }).catch(function(error) {
+        console.log(error);
     });
-  var teachersID = db.collection("teachers").doc(email);
-  teachersID.get().then(function(doc){
-    if (doc.exists) {
-      var data = doc.data();
-      console.log(data);
-      for (var i = 0; i < data.tests.length; i++) {
-        const indivTests = `
+    var user = firebase.auth().currentUser;
+    var teachers = db.collection("teachers");
+    if (user != null) {
+        user.providerData.forEach(function(profile) {
+            email = profile.email;
+        });
+        var teachersID = db.collection("teachers").doc(email);
+        teachersID.get().then(function(doc) {
+            if (doc.exists) {
+                var data = doc.data();
+                console.log(data);
+                for (var i = 0; i < data.tests.length; i++) {
+                    const indivTests = `
         <div class="row">
     <div class="col s12 m6">
       <div class="card blue-grey darken-1">
@@ -43,19 +43,18 @@ function showTests() {
       </div>
     </div>
   </div>
-
         `;
-        html += indivTests;
-      }
-      testti.innerHTML = html;
+                    html += indivTests;
+                }
+                testti.innerHTML = html;
 
 
-    } else {
-      console.log("Doc doesn't exist");
+            } else {
+                console.log("Doc doesn't exist");
+            }
+        })
     }
-  })
-}
-//get the array, for each - push to local array
-//push TestID
-//update(
+    //get the array, for each - push to local array
+    //push TestID
+    //update(
 }
